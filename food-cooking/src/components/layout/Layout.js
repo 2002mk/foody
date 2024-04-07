1eba56cb002fc6557ea9f27e33333c1f60f1cafa
2002mk <ganesanmanisha16@gmail.com>
2024-04-07 08:06:14 +0530

    add next file

---
+++ food-cooking/src/components/layout/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from 'Header'
+function layout() {
+
+    return (
+        <>
+            <Header />
+            <h1>layout</h1>
+        </>
+    );
+}
+
+export default layout
